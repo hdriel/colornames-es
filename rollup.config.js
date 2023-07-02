af7178a8dcc09b7f0860649984c479fb18eb6a8c
hdriel <hdriel@gmail.com>
2023-07-03 01:02:58 +0300

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import del from 'rollup-plugin-delete';
 import dts from 'rollup-plugin-dts';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import generatePackageJson from 'rollup-plugin-generate-package-json';
+// import generatePackageJson from 'rollup-plugin-generate-package-json';
 import filesize from 'rollup-plugin-filesize';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
@@ -39,24 +39,24 @@ export default [
             commonjs(),
             typescript({ tsconfig: 'tsconfig.json' }),
             ...(isProd ? [terser()] : []),
-            generatePackageJson({
-                outputFolder: 'dist',
-                baseContents: (pkg) => ({
-                    name: pkg.name,
-                    version: pkg.version,
-                    description: pkg.description,
-                    license: pkg.license,
-                    author: pkg.author,
-                    keywords: pkg.keywords,
-                    bugs: pkg.bugs,
-                    homepage: pkg.homepage,
-                    publishConfig: pkg.publishConfig,
-                    repository: pkg.repository,
-                    type: pkg.type,
-                    module: pkg.module?.replace('dist/', ''),
-                    main: pkg.main.replace('dist/', ''),
-                }),
-            }),
+            // generatePackageJson({
+            //     outputFolder: 'dist',
+            //     baseContents: (pkg) => ({
+            //         name: pkg.name,
+            //         version: pkg.version,
+            //         description: pkg.description,
+            //         license: pkg.license,
+            //         author: pkg.author,
+            //         keywords: pkg.keywords,
+            //         bugs: pkg.bugs,
+            //         homepage: pkg.homepage,
+            //         publishConfig: pkg.publishConfig,
+            //         repository: pkg.repository,
+            //         type: pkg.type,
+            //         module: pkg.module?.replace('dist/', ''),
+            //         main: pkg.main.replace('dist/', ''),
+            //     }),
+            // }),
             filesize(),
         ],
     },
