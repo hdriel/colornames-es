2f1a08d3dcc35d719cae0fad6336191c4a7039fe
hdriel <hdriel@gmail.com>
2023-07-03 00:52:50 +0300

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import del from 'rollup-plugin-delete';
 import dts from 'rollup-plugin-dts';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import filesize from 'rollup-plugin-filesize';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
@@ -8,14 +12,13 @@ const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
 
 const isProd = process.env.NODE_ENV === 'production';
-const sourcemap = isProd ? undefined : 'inline';
 
 export default [
     {
         input: 'src/index.ts', // Update with the path to your entry file
         output: [
             {
-                sourcemap,
+                sourcemap: 'inline',
                 file: packageJson.main,
                 format: 'cjs',
                 exports: 'named',
@@ -23,14 +26,39 @@ export default [
             },
             // ES2015 modules version so consumers can tree-shake
             {
-                sourcemap,
+                sourcemap: 'inline',
                 file: packageJson.module,
                 format: 'es',
                 exports: 'named',
                 interop: 'esModule',
             },
         ],
-        plugins: [del({ targets: 'dist/*' }), typescript({ tsconfig: 'tsconfig.json' }), ...(isProd ? [terser()] : [])],
+        plugins: [
+            del({ targets: 'dist/*' }),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: 'tsconfig.json' }),
+            ...(isProd ? [terser()] : []),
+            generatePackageJson({
+                outputFolder: 'dist',
+                baseContents: (pkg) => ({
+                    name: pkg.name,
+                    version: pkg.version,
+                    description: pkg.description,
+                    license: pkg.license,
+                    author: pkg.author,
+                    keywords: pkg.keywords,
+                    bugs: pkg.bugs,
+                    homepage: pkg.homepage,
+                    publishConfig: pkg.publishConfig,
+                    repository: pkg.repository,
+                    type: pkg.type,
+                    module: pkg.module?.replace('dist/', ''),
+                    main: pkg.main.replace('dist/', ''),
+                }),
+            }),
+            filesize(),
+        ],
     },
     {
         input: 'dist/index.d.ts',
